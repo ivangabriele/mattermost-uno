734f53a56effc81f839ccfcdb29be7239cb482cc
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-09-09 00:41:38 +0200

    background: make it browser-agnostic

--- src/background/index.js
+++ src/background/index.js
@@ -1,36 +1,41 @@
+// eslint-disable-next-line func-names
+window.browser = (function() {
+  return window.browser || window.chrome;
+})();
+
 let isEnabled = false;
 
 function enable(tabId) {
-  chrome.tabs.insertCSS(tabId, { file: "content.css" });
-  chrome.tabs.executeScript(tabId, { file: "content.js" });
+  window.browser.tabs.insertCSS(tabId, { file: "content.css" });
+  window.browser.tabs.executeScript(tabId, { file: "content.js" });
 
-  chrome.browserAction.setTitle({ tabId, title: "Disable Mattermost Uno" });
-  chrome.browserAction.setIcon({ tabId, path: "icons/icon-32x32.png" });
+  window.browser.browserAction.setTitle({ tabId, title: "Disable Mattermost Uno" });
+  window.browser.browserAction.setIcon({ tabId, path: "icons/icon-32x32.png" });
 
   isEnabled = true;
 }
 
 function disable(tabId) {
-  chrome.tabs.executeScript(tabId, { code: `location.reload();` });
+  window.browser.tabs.executeScript(tabId, { code: `location.reload();` });
 
-  chrome.browserAction.setTitle({ tabId, title: "Enable Mattermost Uno" });
-  chrome.browserAction.setIcon({ tabId, path: "icons/icon-32x32-bw.png" });
+  window.browser.browserAction.setTitle({ tabId, title: "Enable Mattermost Uno" });
+  window.browser.browserAction.setIcon({ tabId, path: "icons/icon-32x32-bw.png" });
 
   isEnabled = false;
 }
 
-chrome.browserAction.onClicked.addListener(tab => {
-  chrome.tabs.sendMessage(tab.id, "areYouThere", async res => {
+window.browser.browserAction.onClicked.addListener(tab => {
+  window.browser.tabs.sendMessage(tab.id, "areYouThere", async res => {
     if (res === undefined) enable(tab.id);
     else disable(tab.id);
   });
 });
 
-chrome.tabs.onUpdated.addListener((tabId, changeInfo) => {
+window.browser.tabs.onUpdated.addListener((tabId, changeInfo) => {
   const { status } = changeInfo;
   if (status !== "complete") return;
 
-  chrome.tabs.sendMessage(tabId, "areYouThere", async res => {
+  window.browser.tabs.sendMessage(tabId, "areYouThere", async res => {
     if (isEnabled && res === undefined) enable(tabId);
   });
 });
