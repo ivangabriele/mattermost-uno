2a9bc37539a060f929209358335416f502a7605f
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-09-08 23:33:36 +0200

    background: integrate enabling/disabling when the extension icon is clicked

--- src/background/index.js
+++ src/background/index.js
@@ -1,16 +1,36 @@
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+let isEnabled = false;
+
+function enable(tabId) {
+  chrome.tabs.insertCSS(tabId, { file: "content.css" });
+  chrome.tabs.executeScript(tabId, { file: "content.js" });
+
+  chrome.browserAction.setTitle({ tabId, title: "Disable Mattermost Uno" });
+  chrome.browserAction.setIcon({ tabId, path: "icons/icon-32x32.png" });
+
+  isEnabled = true;
+}
+
+function disable(tabId) {
+  chrome.tabs.executeScript(tabId, { code: `location.reload();` });
+
+  chrome.browserAction.setTitle({ tabId, title: "Enable Mattermost Uno" });
+  chrome.browserAction.setIcon({ tabId, path: "icons/icon-32x32-bw.png" });
+
+  isEnabled = false;
+}
+
+chrome.browserAction.onClicked.addListener(tab => {
+  chrome.tabs.sendMessage(tab.id, "areYouThere", async res => {
+    if (res === undefined) enable(tab.id);
+    else disable(tab.id);
+  });
+});
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo) => {
   const { status } = changeInfo;
   if (status !== "complete") return;
 
-  const { url } = tab;
-  if (url === undefined || !url.startsWith("https://mattermost.")) return;
-
-  if (chrome.runtime.lastError) return;
-
   chrome.tabs.sendMessage(tabId, "areYouThere", async res => {
-    if (res === undefined) {
-      chrome.tabs.insertCSS({ file: "content.css" });
-      chrome.tabs.executeScript({ file: "content.js" });
-    }
+    if (isEnabled && res === undefined) enable(tabId);
   });
 });
