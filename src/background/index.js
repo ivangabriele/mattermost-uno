c666c31f8807fc380d22a60c118dbe2bf2cb2904
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-09-09 09:32:05 +0200

    background: remove useless browser-agnostic hack

--- src/background/index.js
+++ src/background/index.js
@@ -1,41 +1,42 @@
-// eslint-disable-next-line func-names
-window.browser = (function() {
-  return window.browser || window.chrome;
-})();
-
 let isEnabled = false;
 
-function enable(tabId) {
-  window.browser.tabs.insertCSS(tabId, { file: "content.css" });
-  window.browser.tabs.executeScript(tabId, { file: "content.js" });
+let tabs = [];
 
-  window.browser.browserAction.setTitle({ tabId, title: "Disable Mattermost Uno" });
-  window.browser.browserAction.setIcon({ tabId, path: "icons/icon-32x32.png" });
+function enable(tabId) {
+  if (!tabs.includes(tabId)) tabs.push(tabId);
+
+  chrome.tabs.insertCSS(tabId, { file: "content.css" });
+  chrome.tabs.executeScript(tabId, { file: "content.js" });
+
+  chrome.browserAction.setTitle({ tabId, title: "Disable Mattermost Uno" });
+  chrome.browserAction.setIcon({ tabId, path: "icons/icon-32x32.png" });
 
   isEnabled = true;
 }
 
 function disable(tabId) {
-  window.browser.tabs.executeScript(tabId, { code: `location.reload();` });
+  if (!tabs.includes(tabId)) tabs = tabs.filter(id => id !== tabId);
 
-  window.browser.browserAction.setTitle({ tabId, title: "Enable Mattermost Uno" });
-  window.browser.browserAction.setIcon({ tabId, path: "icons/icon-32x32-bw.png" });
+  chrome.tabs.executeScript(tabId, { code: `location.reload();` });
+
+  chrome.browserAction.setTitle({ tabId, title: "Enable Mattermost Uno" });
+  chrome.browserAction.setIcon({ tabId, path: "icons/icon-32x32-bw.png" });
 
   isEnabled = false;
 }
 
-window.browser.browserAction.onClicked.addListener(tab => {
-  window.browser.tabs.sendMessage(tab.id, "areYouThere", async res => {
+chrome.browserAction.onClicked.addListener(tab => {
+  chrome.tabs.sendMessage(tab.id, "areYouThere", async res => {
     if (res === undefined) enable(tab.id);
     else disable(tab.id);
   });
 });
 
-window.browser.tabs.onUpdated.addListener((tabId, changeInfo) => {
+chrome.tabs.onUpdated.addListener((tabId, changeInfo) => {
   const { status } = changeInfo;
   if (status !== "complete") return;
 
-  window.browser.tabs.sendMessage(tabId, "areYouThere", async res => {
+  chrome.tabs.sendMessage(tabId, "areYouThere", async res => {
     if (isEnabled && res === undefined) enable(tabId);
   });
 });
