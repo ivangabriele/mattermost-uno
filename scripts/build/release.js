345000818cf70d3e70ae99c2f6fc7e4fe0588f26
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-09-09 12:22:47 +0200

    scripts/build/release: add source code archive

--- scripts/build/release.js
+++ scripts/build/release.js
@@ -33,33 +33,95 @@ execSync(`npm version ${newVersionLevel}`);
   Unsigned Archive
 */
 
-const archiveOutput = createWriteStream(`./mattermost-uno-unsigned.zip`);
-const archive = archiver("zip", { zlib: { level: 9 } });
+function buildUnsignedArchive() {
+  const archiveOutput = createWriteStream(`./mattermost-uno-unsigned.zip`);
+  const archive = archiver("zip", { zlib: { level: 9 } });
 
-// Listen for all archive data to be written:
-// The 'close' event is fired only when a file descriptor is involved.
-archiveOutput.on("close", () => {
-  console.info(`Archive size: ${numeral(archive.pointer()).format("0.00 b")}.`);
-});
+  // Listen for all archive data to be written:
+  // The 'close' event is fired only when a file descriptor is involved.
+  archiveOutput.on("close", () => {
+    console.info(`Unsigned Archive size: ${numeral(archive.pointer()).format("0.00 b")}.`);
+  });
 
-// This event is fired when the data source is drained no matter what was the data source.
-// It is not part of this library but rather from the NodeJS Stream API.
-// @see: https://nodejs.org/api/stream.html#stream_event_end
-archiveOutput.on("end", () => console.warn("Warning: Data has been drained"));
+  // This event is fired when the data source is drained no matter what was the data source.
+  // It is not part of this library but rather from the NodeJS Stream API.
+  // @see: https://nodejs.org/api/stream.html#stream_event_end
+  archiveOutput.on("end", () => console.warn("Warning: Data has been drained"));
 
-archive.on("error", err => {
-  console.error(`Error: ${err.message}`);
-  process.exit(1);
-});
-archive.on("warning", err => {
-  if (err.code === "ENOENT") {
-    console.warn(`Warning: ${err.message}`);
-  } else {
+  archive.on("error", err => {
     console.error(`Error: ${err.message}`);
     process.exit(1);
-  }
-});
+  });
+  archive.on("warning", err => {
+    if (err.code === "ENOENT") {
+      console.warn(`Warning: ${err.message}`);
+    } else {
+      console.error(`Error: ${err.message}`);
+      process.exit(1);
+    }
+  });
 
-archive.pipe(archiveOutput);
-archive.glob("**", { cwd: "./dist" });
-archive.finalize();
+  archive.pipe(archiveOutput);
+  archive.glob("**", { cwd: "./dist" });
+  archive.finalize();
+}
+
+/* -------------------------------------------------------------------------------------------------
+  Source Code Archive
+*/
+
+function buildSourceCodeArchive() {
+  const archiveOutput = createWriteStream(`./mattermost-uno-${newVersion}.zip`);
+  const archive = archiver("zip", { zlib: { level: 0 } });
+
+  // Listen for all archive data to be written:
+  // The 'close' event is fired only when a file descriptor is involved.
+  archiveOutput.on("close", () => {
+    console.info(`Source Code Archive size: ${numeral(archive.pointer()).format("0.00 b")}.`);
+  });
+
+  // This event is fired when the data source is drained no matter what was the data source.
+  // It is not part of this library but rather from the NodeJS Stream API.
+  // @see: https://nodejs.org/api/stream.html#stream_event_end
+  archiveOutput.on("end", () => console.warn("Warning: Data has been drained"));
+
+  archive.on("error", err => {
+    console.error(`Error: ${err.message}`);
+    process.exit(1);
+  });
+  archive.on("warning", err => {
+    if (err.code === "ENOENT") {
+      console.warn(`Warning: ${err.message}`);
+    } else {
+      console.error(`Error: ${err.message}`);
+      process.exit(1);
+    }
+  });
+
+  archive.pipe(archiveOutput);
+
+  archive.glob("icons/**");
+  archive.glob("src/**");
+  archive.glob("*.js");
+  archive.glob("*.json");
+  archive.glob("*.md");
+
+  archive.file(".babelrc");
+  archive.file(".editorconfig");
+  archive.file(".eslintrc.yml");
+  archive.file(".prettierrc");
+  archive.file("LICENSE");
+  archive.file("yarn.lock");
+
+  archive.finalize();
+}
+
+/* -------------------------------------------------------------------------------------------------
+  Run
+*/
+
+// eslint-disable-next-line func-names
+(async function() {
+  buildUnsignedArchive();
+  buildSourceCodeArchive();
+})();
