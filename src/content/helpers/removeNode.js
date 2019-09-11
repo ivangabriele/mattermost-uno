e5cb82eaed639d6930a5f67821f082903ffcbf10
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-09-11 16:49:31 +0200

    content/helpers/removeNode: delete

--- src/content/helpers/removeNode.js
+++
@@ -1,7 +0,0 @@
-export default function removeNode($node) {
-  if ($node.parentNode === null) {
-    throw new Error("helpers/removeNode(): The parent node is null.");
-  }
-
-  $node.parentNode.removeChild($node);
-}
